a4f14e79ee5bd50202f2ccccac27ab38b20fb7e1
asdfkjll <124416006+asdfkjll@users.noreply.github.com>
2026-08-04 18:46:42 -0400

    more products

--- src/ui/menu.js
+++ src/ui/menu.js
@@ -1,3 +1,4 @@
+import './menu.css'
 import { createElementFromHTML, createSection } from './ui.js'
 import buttonComponent from './button.js'
 import menu from '../services/menu-service.json'
@@ -17,6 +18,13 @@ function menuItemComponent(item) {
       </div>
     </div>
   `)
+
+  if(item.offer) {
+    const offerTag = createElementFromHTML(`
+      <span class="menu-item__offer-tag">OFFER!</span>  
+    `)
+    itemElement.querySelector('.menu-item__content').appendChild(offerTag)
+  }
 
   itemElement.querySelector('.menu-item__content').appendChild(
     buttonComponent({
